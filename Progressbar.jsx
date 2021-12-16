a827250e38010753f158ffa7debc2f426a25bd8c
Nate Lovell <Natelovell99@gmail.com>
2021-12-15 21:40:16 -0700

    Create Progressbar.jsx

---
+++ Progressbar.jsx
@@ -0,0 +1,28 @@
+﻿var window = new Window("palette", "progressBar", undefined);
+window.orientation = "column";
+
+var editTextOne = window.add("edittext", undefined, "");
+editTextOne.size = [60, 25];
+
+var editTextTwo = window.add("edittext", undefined, "");
+editTextTwo.size = [60, 25];
+
+var editTextThree = window.add("edittext", undefined, "");
+editTextThree.size = [60, 25];
+
+var progress = window.add("progressbar", undefined, "");
+
+window.center();
+window.show();
+
+editTextOne.onActivate = function() {
+    progress.value = 33;
+    }
+
+editTextTwo.onActivate = function() {
+    progress.value = 66;
+    }
+
+editTextThree.onActivate = function() {
+    progress.value = 100;
+    }
